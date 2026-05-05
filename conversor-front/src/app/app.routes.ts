175fa3f2a35ef3d1bf6865abc8c5ea05582ca6f5
isidcas <isidca>
2026-05-05 17:49:23 +0200

    configurando rutas

--- conversor-front/src/app/app.routes.ts
+++ conversor-front/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { Login } from './pages/login/login';
+import { Register } from './pages/register/register';
+import { Dashboard } from './pages/dashboard/dashboard';
+import { Historial } from './pages/historial/historial';
+import { Contacto } from './pages/contacto/contacto';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+{path:'', component: Login },
+  { path:'registro', component: Register },
+  { path:'dashboard', component: Dashboard },
+  { path:'historial', component: Historial },
+  { path:'contacto', component: Contacto }
+
+];
