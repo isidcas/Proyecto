9d8ff45d4f794ffbf8e442d41141fd512da0fcd1
isidcas <isidca>
2026-05-11 19:06:59 +0200

    commit

--- conversor-front/src/app/services/auth.ts
+++ conversor-front/src/app/services/auth.ts
@@ -1,6 +1,14 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
-export class Auth {}
+export class Auth {
+  registro(datos:any){
+  return this.http.post(
+    'http://localhost/Proyect/public/register.php',
+    datos
+  );
+}
+
+}
