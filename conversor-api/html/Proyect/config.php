596bc7d58b3dd852b1a329f22dac20d0c3d4c573
isidcas <isidca>
2026-05-22 11:41:37 +0200

    a

--- conversor-api/html/Proyect/config.php
+++ conversor-api/html/Proyect/config.php
@@ -1,11 +1,12 @@
 <?php
-    return [
-        'db'=>[
-            'host'=>'conversor-api-db-1',
-            'dbname'=>'conversor_divisas',
-            'user'=>'root',
-            'pass'=>'root',
-            'port'=>3306
-        ]
+
+return [
+    'db' => [
+        // getenv busca las variables que configuramos en Render. Si no existen, usa tu Docker local
+        'host'   => getenv('DB_HOST') ?: 'conversor-api-db-1',
+        'dbname' => getenv('DB_NAME') ?: 'conversor_divisas',
+        'user'   => getenv('DB_USER') ?: 'root',
+        'pass'   => getenv('DB_PASS') ?: 'root',
+        'port'   => getenv('DB_PORT') ?: '3306'
     ]
-?>
+];
